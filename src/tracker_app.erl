101aec0b5ca2d62977a9798ad7fe18a18d4371d5
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2016-08-23 15:25:45 +0200

    Run basic Cowboy websocket demo from within the rebar3 project

--- src/tracker_app.erl
+++ src/tracker_app.erl
@@ -1,26 +1,34 @@
-%%%-------------------------------------------------------------------
 %% @doc tracker public API
 %% @end
-%%%-------------------------------------------------------------------
 
 -module(tracker_app).
 
 -behaviour(application).
 
 %% Application callbacks
--export([start/2, stop/1]).
+-export([start/2,
+         stop/1]).
 
-%%====================================================================
+%%
 %% API
-%%====================================================================
+%%
 
 start(_StartType, _StartArgs) ->
+    Routes = [
+              {'_', [
+                     {"/", cowboy_static, {priv_file, tracker, "index.html"}},
+                     {"/websocket", tr_device_location_stream, []},
+                     {"/static/[...]", cowboy_static, {priv_dir, tracker, "static"}}
+                    ]}
+             ],
+    Dispatch = cowboy_router:compile(Routes),
+    {ok, _} = cowboy:start_http(http, 100, [{port, 8080}],
+                                [{env, [{dispatch, Dispatch}]}]),
     tracker_sup:start_link().
 
-%%--------------------------------------------------------------------
 stop(_State) ->
     ok.
 
-%%====================================================================
+%%
 %% Internal functions
-%%====================================================================
+%%
