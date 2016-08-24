4271ef1aff7af57418b8aafe7880a2279b04de38
Radek Szymczyszyn <radoslaw.szymczyszyn@erlang-solutions.com>
2016-08-24 18:28:25 +0200

    Allow to customize listening port

--- src/tracker_app.erl
+++ src/tracker_app.erl
@@ -24,7 +24,8 @@ start(_StartType, _StartArgs) ->
                     ]}
              ],
     Dispatch = cowboy_router:compile(Routes),
-    {ok, _} = cowboy:start_http(http, 100, [{port, 8080}],
+    {ok, _} = cowboy:start_http(http, 100,
+                                [{port, maps:get(listen_port, opts())}],
                                 [{env, [{dispatch, Dispatch}]}]),
     tracker_sup:start_link(defaults()).
 
@@ -32,7 +33,15 @@ stop(_State) ->
     ok.
 
 defaults() ->
-    #{}.
+    #{listen_port => 7890}.
+
+opts() ->
+    opts([]).
+
+opts(LOverrides) ->
+    Overrides = maps:from_list(LOverrides),
+    Env = maps:from_list(application:get_all_env(tracker)),
+    maps:merge(maps:merge(defaults(), Env), Overrides).
 
 %%
 %% Internal functions
