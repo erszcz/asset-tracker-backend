a7ba8f7f0cb8b38df478bfdef2d80cdac626bf91
Bartosz Szafran <bartosz.szafran@erlang-solutions.com>
2016-08-26 13:16:42 +0200

    Add sample data to endpoint /devices

--- src/tr_devices_handler.erl
+++ src/tr_devices_handler.erl
@@ -22,17 +22,37 @@ content_types_provided(Req, State) ->
 
 to_json(Req0, State) ->
     {IsPretty, Req1} = cowboy_req:qs_val(<<"pretty">>, Req0),
-    DevicesList = get_devices(),
-    Reply = get_reply(DevicesList, IsPretty),
-    {Reply, Req1, State}.
+    {Limit, Req2} = cowboy_req:qs_val(<<"limit">>, Req1, <<"1">>),
+%    DevicesList = get_devices(Limit),
+    TestDevices = [get_sample_device(<<"rower">>),
+               get_sample_device(<<"pies">>)],
+    Reply = get_reply(TestDevices, IsPretty),
+    {Reply, Req2, State}.
 
 %% Internal
 
-get_devices() ->
-    [this, is, my, device].
+get_devices(LimitBin) ->
+    Limit = erlang:binary_to_integer(LimitBin),
+    {ok, Devices} = location_store:get_devices(),
+    get_devices_with_location(Devices, Limit).
+
+get_devices_with_location(Devices, Limit) ->
+    Locations = lists:map(
+                  fun(Device) ->
+                          location_store:get_current_locations(Device, Limit)
+                  end,
+                  Devices),
+    lists:zip(Devices, Locations).
 
 get_reply(DevicesList, <<"true">>) ->
-    tr_json:pretty_encode(#{nodes => DevicesList});
+    tr_json:pretty_encode(DevicesList);
 
 get_reply(DevicesList, _) ->
-    tr_json:encode(#{nodes => DevicesList}).
+    tr_json:encode(DevicesList).
+
+get_sample_device(Name) ->
+    #{<<"display_name">> => Name,
+      <<"data">> => <<"50.066902,19.929014">>,
+      <<"ttl">> => <<"60">>,
+      <<"published_at">> => <<"2016-08-17T10:28:03.512Z">>,
+      <<"coreid">> => <<"4f0047001951343334363036">>}.
